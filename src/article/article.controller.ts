ace90a415c775f6c2b81a32c0f4921276d9d6039
allblueee <allblueee798@gmail.com>
2023-03-02 23:32:19 +0800

    feat: add some api

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { GetUser } from "src/auth/decorator";
 import { JwtGuard } from "src/guard";
@@ -16,6 +16,27 @@ export class ArticleController {
             title: dto.title,
             content: dto.content,
         })
+    }
+    @Post('get')
+    getArticle(@GetUser('') user: User, @Body() getArticle) {
+        return this.articleService.getArticle(getArticle.articleId)
+    }
+    @Post('update')
+    // 这里发现 body 好像只能接一个参数啊
+    updateArticle(@GetUser('') user: User, @Body() updateArticle) {
+        return this.articleService.updateArticle(updateArticle.articleId, {
+            id: user.id,
+            title: updateArticle.dto.title,
+            content: updateArticle.dto.content,
+        })
+    }
+    @Post('delete')
+    deleteArticle(@GetUser('') user: User, @Body() deleteArticle) {
+        return this.articleService.deleteArticle(deleteArticle.articleId)
+    }
+    @Get('getAllPublished')
+    getAllPublishedArticle(@GetUser('') user: User) {
+        return this.articleService.getAllPublishedArticles(user.id);
     }
 
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -5,6 +5,7 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { ArticleDto } from './dto';
+import { error } from 'console';
 
 @Injectable()
 export class ArticleService {
@@ -26,4 +27,47 @@ export class ArticleService {
             throw error;
         }
     }
+    async getArticle(articleId) {
+        const article = await this.prisma.article.findUnique({
+            where: {
+                id: parseInt(articleId)
+            }
+        })
+        if (!article) {
+            throw error;
+        }
+        return article;
+    }
+    async updateArticle(articleId, dto: ArticleDto) {
+        try {
+            const article = await this.prisma.article.update({
+                where: { id: parseInt(articleId) },
+                data: {
+                    userId: dto.id,
+                    title: dto.title,
+                    content: dto.content
+                },
+            });
+            return article;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+    async deleteArticle(articleId) {
+        try {
+            const article = await this.prisma.article.delete({
+                where: { id: parseInt(articleId) },
+            });
+            return article;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+    async getAllPublishedArticles(userId) {
+        const publishedArticles = await this.prisma.article.findMany({ where: { userId: userId } })
+        return publishedArticles;
+    }
+
 }
